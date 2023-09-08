0b9bdfeaac4d61b10f7f1e348a34fc3963428692
Deivistor <143622024+Deivistor@users.noreply.github.com>
2023-09-08 08:49:49 -0600

    Add files via upload

---
+++ AreaTriangulo1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package areatriangulo1;
+
+/**
+ *
+ * @author Usuario
+ */
+public class AreaTriangulo1
+{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        //numeros enteros
+        //numeros decimales
+        
+        int base = 10;
+        int Altura = 56;
+        int area = base*Altura/2;
+        System.out.println("El area del traingulo es: " + area);
+    }
+    
+}
